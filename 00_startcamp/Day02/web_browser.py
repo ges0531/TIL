5e98e54787c9e49b46b9d0193dc971be836bc71e
eung su <dmstndlekd@gmail.com>
2019-07-09 14:43:18 +0900

    startcamp | finish webbrowser | 190709

---
+++ 00_startcamp/Day02/web_browser.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+
+urls = [
+    'www.github.com',
+    'www.google.com',
+    'www.youtube.com',
+    'www.slack.com',
+    'www.naver.com'
+    ]
+
+for url in urls:
+    webbrowser.open(url)
