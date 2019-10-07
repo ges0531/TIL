0550c3c335fd1b5adb3ad3bd482eac1084fb9990
Eunsu <dmstndlekd@gmail.com>
2019-10-07 10:32:57 +0900

    191007| back_up

---
+++ Algorithm/191004/카드2/카드카드카드.py
@@ -0,0 +1,19 @@
+import collections
+import sys
+
+sys.stdin = open('input.txt', 'r')
+
+N = int(input())
+opt = 1
+q = collections.deque()
+for i in range(1, N+1):
+    q.append(i)
+while len(q) != 1:
+    if opt == 1:
+        q.popleft()
+        opt = 0
+    elif opt == 0:
+        a = q.popleft()
+        q.append(a)
+        opt = 1
+print(q[0])
