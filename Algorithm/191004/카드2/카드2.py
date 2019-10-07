7bb59de41023c15920ab4ff19a7b2a2c114a2b8b
Eunsu <dmstndlekd@gmail.com>
2019-10-07 09:06:44 +0900

    191007| Algorithm | stack, queue

--- Algorithm/191004/카드2/카드2.py
+++ Algorithm/191004/카드2/카드2.py
@@ -6,8 +6,10 @@ N = int(input())
 
 num_list = [i+1 for i in range(N)]
 a = [0]
-while num_list:
-    num_list.pop(0)
-    a[0] = num_list.pop(0)
-    num_list = num_list + a
-print(num_list[0])
+index = 0
+for _ in range(N-1):
+    index += 1
+    a[0] = num_list[index]
+    index += 1
+    num_list = num_list+a
+print(num_list[-1])
