4cd304c6151ddd6fa3917ec2f7a0104df8002e0c
Eunsu <dmstndlekd@gmail.com>
2019-10-25 07:58:02 +0900

    modified_model

--- extraclass/OCT_VICTORY/accounts/models.py
+++ extraclass/OCT_VICTORY/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# 'auth.User 모델을 사용 => django 기본 제공 Model
+# accounts.models 에서는 할일 x
+# models => forms => urls + views + templates
+# 우리가 사용할 회원가입/로그인 form 은 생성?
+# django.contrib/auth.forms 에 다 준비되어 있음
+# models(x) => forms(x) => urls + views + templates
