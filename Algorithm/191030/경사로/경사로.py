17024f9441e10eaf6cd0742f2060208e815dd66c
Eunsu <dmstndlekd@gmail.com>
2019-10-31 20:18:09 +0900

    191031|Algorithm|runway

--- Algorithm/191030/경사로/경사로.py
+++ Algorithm/191030/경사로/경사로.py
@@ -5,47 +5,45 @@ sys.stdin = open('input.txt', 'r')
 size, length = map(int, input().split())
 road_matrix = [list(map(int, input().split())) for _ in range(size)]
 count = 0
-# for row in range(size):
-#     column_flag = 0
-#     a = road_matrix[row][column_flag]
-#     flag = 1
-#     visited = [0]*size
-#     while column_flag < size-1:
-#         if flag == 0:
-#             break
-#         if a == road_matrix[row][column_flag+1]:
-#             column_flag += 1
-#         elif a - 1 == road_matrix[row][column_flag+1]:
-#             flag = 1
-#             for i in range(1, length+1):
-#                 if road_matrix[row][column_flag+i] != a-1 or column_flag+length >= size:
-#                     flag = 0
-#                     break
-#             if flag:
-#                 for ii in range(1, length + 1):
-#                     visited[column_flag + ii] = 1
-#                 a = a-1
-#                 column_flag += (length-1)
-#         elif a + 1 == road_matrix[row][column_flag + 1]:
-#             flag = 1
-#             for i in range(length):
-#                 if road_matrix[row][column_flag - i] != a or visited[column_flag - i]:
-#                     flag = 0
-#                     break
-#             if flag:
-#                 for ii in range(1, length):
-#                     visited[column_flag - ii] = a+1
-#                 a = a + 1
-#                 column_flag += 1
-#         else:
-#             flag = 0
-#     if flag:
-#         count += 1
+for row in range(size):
+    column_flag = 0
+    a = road_matrix[row][column_flag]
+    flag = 1
+    visited = [0]*size
+    while column_flag < size-1:
+        if flag == 0:
+            break
+        if a == road_matrix[row][column_flag+1]:
+            column_flag += 1
+        elif a - 1 == road_matrix[row][column_flag+1]:
+            flag = 1
+            for i in range(1, length+1):
+                if road_matrix[row][column_flag+i] != a-1 or column_flag+length >= size:
+                    flag = 0
+                    break
+            if flag:
+                for ii in range(1, length + 1):
+                    visited[column_flag + ii] = 1
+                a = a-1
+                column_flag += (length-1)
+        elif a + 1 == road_matrix[row][column_flag + 1]:
+            flag = 1
+            for i in range(length):
+                if road_matrix[row][column_flag - i] != a or visited[column_flag - i] or column_flag-(length-1) < 0:
+                    flag = 0
+                    break
+            if flag:
+                a = a + 1
+                column_flag += 1
+        else:
+            flag = 0
+    if flag:
+        count += 1
 for column in range(size):
     row_flag = 0
     a = road_matrix[row_flag][column]
     flag = 1
-    visited = [0] * size
+    visited = [0]*size
     while row_flag < size-1:
         if flag == 0:
             break
@@ -62,10 +60,10 @@ for column in range(size):
                     visited[row_flag + jj] = 1
                 a = a-1
                 row_flag += (length-1)
-        elif a + 1 == road_matrix[row_flag + 1][column]:
+        elif a + 1 == road_matrix[row_flag+1][column]:
             flag = 1
             for j in range(length):
-                if visited[row_flag-j] != a or visited[row_flag - j]:
+                if road_matrix[row_flag-j][column] != a or visited[row_flag - j] or row_flag-(length-1) < 0:
                     flag = 0
                     break
             if flag:
@@ -73,7 +71,6 @@ for column in range(size):
                 row_flag += 1
         else:
             flag = 0
-    if flag == 1:
+    if flag:
         count += 1
-        print(column, count)
 print(count)
