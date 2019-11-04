db0c48b641aefc18119a2421c69ef252104de9fb
Eunsu <dmstndlekd@gmail.com>
2019-11-04 17:40:03 +0900

    191104|Algorithm| country_people

--- Algorithm/191101/인구이동/인구이동.py
+++ Algorithm/191101/인구이동/인구이동.py
@@ -4,12 +4,14 @@ sys.stdin = open('input.txt', 'r')
 
 
 def BFS(start_node):
-    global visited
+    global visited, my_list, my_sum, my_count, flag
     queue = [start_node]
     visited[start_node[0]][start_node[1]] = 1
-    flag = 0
     while queue:
         a = queue.pop(0)
+        my_list.append(a)
+        my_sum += country_matrix[a[0]][a[1]]
+        my_count += 1
         for i in range(4):
             y = a[0]
             x = a[1]
@@ -18,30 +20,28 @@ def BFS(start_node):
             if 0 <= idy < size and 0 <= idx < size:
                 if not visited[idy][idx]:
                     if people_min <= abs(country_matrix[y][x]-country_matrix[idy][idx]) <= people_max:
-                        visited[idy][idx] = 1
+                        visited[idy][idx] = visited[y][x]
                         queue.append([idy, idx])
-                        division_count += 1
-                        my_sum += country_matrix[idy][idx]
-                        location_list.append([idy, idx])
-                        flag = 1
-
-    if flag:
-        return True
+                        flag += 1
 
 
 size, people_min, people_max = map(int, input().split())
 country_matrix = [list(map(int, input().split())) for _ in range(size)]
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
-count = 1
-result = 0
-while count:
-    count = 0
+count = 0
+flag = 1
+while flag:
+    flag = 0
     visited = [[0] * size for _ in range(size)]
     for row in range(len(country_matrix)):
         for column in range(len(country_matrix[row])):
-            if BFS([row, column]):
-                count += 1
-    result += count
-    print(count)
-print(result)
+            if not visited[row][column]:
+                my_list = []
+                my_sum = 0
+                my_count = 0
+                BFS([row, column])
+                for j in range(len(my_list)):
+                    country_matrix[my_list[j][0]][my_list[j][1]] = my_sum//my_count
+    count += 1
+print(count-1)
