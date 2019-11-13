f335ae82bb6a566ba85204c4c41643ac32672b36
Eunsu <dmstndlekd@gmail.com>
2019-11-14 08:37:58 +0900

    191114|Algorithm|security

--- Algorithm/191113/숫자만들기/숫자만들기.py
+++ Algorithm/191113/숫자만들기/숫자만들기.py
@@ -2,29 +2,10 @@ import sys
 
 sys.stdin = open('input.txt', 'r')
 
-def calculate(result_list):
 
-    for l in range(len(result_list)):
-        if result_list[l] == '+':
-            result_list[l + 1] = result_list[l-1] + result_list[l+1]
-        elif result_list[l] == '-':
-            result_list[l + 1] = result_list[l - 1] - result_list[l + 1]
-        elif result_list[l] == '*':
-            result_list[l + 1] = result_list[l - 1] * result_list[l + 1]
-        elif result_list[l] == '/':
-            result_list[l + 1] = int(result_list[l - 1] / result_list[l + 1])
-    return result_list[-1]
-
-def perm(k, n, arr):
-    global my_min, my_max
+def perm(k, n, arr, result):
+    global my_min, my_max, result_list
     if k == n:
-        result_list = []
-        for ii in range(n):
-            result_list.append(number_list[ii])
-            result_list.append(arr[ii])
-        result_list.append(number_list[-1])
-        result = calculate(result_list)
-
         if my_min > result:
             my_min = result
         if my_max < result:
@@ -32,8 +13,17 @@ def perm(k, n, arr):
     else:
         for a in range(k, n):
             arr[a], arr[k] = arr[k], arr[a]
-            print(arr)
-            perm(k+1, n, arr)
+            result_2 = result
+            if arr[k] == '+':
+                result = result + number_list[k+1]
+            elif arr[k] == '-':
+                result = result - number_list[k+1]
+            elif arr[k] == '*':
+                result = result * number_list[k+1]
+            elif arr[k] == '/':
+                result = int(result / number_list[k+1])
+            perm(k+1, n, arr, result)
+            result = result_2
             arr[a], arr[k] = arr[k], arr[a]
 
 
@@ -46,10 +36,11 @@ for test_case in range(1, T+1):
     number_list = list(map(int, input().split()))
     operator = ['+', '-', '*', '/']
     operator_list = []
-    my_min = 100000000
-    my_max = -100000000
+    result_list = []
+    my_min = 100000001
+    my_max = -100000001
     for i in range(4):
         for j in range(operator_count[i]):
             operator_list.append(operator[i])
-    perm(0, number_count-1, operator_list)
+    perm(0, len(operator_list), operator_list, number_list[0])
     print('#{} {}'.format(test_case, my_max-my_min))
