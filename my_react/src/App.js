95bd5edd54c89c4dcee770f6e877faa5a9c1863a
Eunsu <dmstndlekd@gmail.com>
2020-01-11 16:48:07 +0900

    200111|React|Prop-types

--- my_react/src/App.js
+++ my_react/src/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name}) {
-  return <h1>I like {name}</h1>
+function Food({name, rating}) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 const foodILike = [{
   id:1,
-  name: "Kimchi"
+  name: "Kimchi",
+  rating: 3.0
 },
 {
   id:2,
-  name: "Kimbap"
+  name: "Kimbap",
+  rating: 4.0
 },
 {
   id:3,
-  name: "bibimbap"
+  name: "bibimbap",
+  rating: 5.0
 }]
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} />
+        <Food key={dish.id} name={dish.name} rating={dish.rating} />
       ))}
     </div>
   );
