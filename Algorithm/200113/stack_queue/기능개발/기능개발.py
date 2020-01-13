23467ab6a180e7864009131250f05f4190b7ce6f
Eunsu <dmstndlekd@gmail.com>
2020-01-13 21:38:23 +0900

    200113|Algorithm|stack_queue_3

--- Algorithm/200113/stack_queue/기능개발/기능개발.py
+++ Algorithm/200113/stack_queue/기능개발/기능개발.py
@@ -1,26 +1,20 @@
 def solution(progresses, speeds):
     answer = []
-    visited = [0]*len(progresses)
-    flag = 1
-    while flag:
+    while progresses:
         for dev_time in range(len(progresses)):
             if progresses[dev_time] < 100:
                 progresses[dev_time] += speeds[dev_time]
-                visited[dev_time] += 1
-                flag = 1
-            else:
-                flag = 0
-    a = visited[0]
-    answer_count = 0
-    while visited:
-        if a >= visited[0]:
-            answer_count += 1
-            visited.pop(0)
-        else:
-            answer.append(answer_count)
-            answer_count = 0
-            a = visited[0]
-    answer.append(answer_count)
+        if progresses[0] >= 100:
+            count = 0
+            while progresses:
+                if progresses[0] >= 100:
+                    progresses.pop(0)
+                    speeds.pop(0)
+                    count += 1
+                else:
+                    break
+            answer.append(count)
+
     return answer
 
 
