3469924f855662be9ba743ae41c6fc349bd8f37b
김은수 <dmstndlekd@gmail.com>
2020-01-14 08:18:04 +0900

    200114|React|react_practice

--- my_react/README.md
+++
@@ -1,1 +0,0 @@
-# React my_app
---
+++ react_pratice/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import axios from "axios";
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log('hello');
+  }
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+  }
+}
+
+export default App;
