c8b6d00308b4ef839449c9d6c15d6d918620ef1f
김은수 <dmstndlekd@gmail.com>
2020-01-14 17:56:52 +0900

    200114|React|Counter

--- 12_react_button/src/index.js
+++ 12_react_button/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Counter(props) {
+    return (
+        <button className="counter" onClick={props.onClick}>
+            +
+        </button>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Number extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+}
+
+
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
