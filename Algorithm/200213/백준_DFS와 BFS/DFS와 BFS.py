a354084157d43c5efbeea5eff524c2c6c295bbe3
김은수 <dmstndlekd@gmail.com>
2020-02-14 08:35:03 +0900

    200214|Algorithm|DFS_BFS_solve

--- Algorithm/200213/백준_DFS와 BFS/DFS와 BFS.py
+++ Algorithm/200213/백준_DFS와 BFS/DFS와 BFS.py
@@ -4,24 +4,26 @@ sys.stdin = open('input.txt', 'r')
 
 
 def DFS(flag_node):
-    # print(flag_node, end=' ')
+    global visited
+    print(flag_node, end=' ')
     for j in range(len(my_list[flag_node])):
-        if not visited[jj]:
-            visited[jj] = 1
-            DFS(jj)
+        if not visited[my_list[flag_node][j]]:
+            visited[my_list[flag_node][j]] = 1
+            DFS(my_list[flag_node][j])
+
 
 def BFS(flag_node):
+    global my_list
     queue = [flag_node]
     visited_2 = [0]*(node_count+1)
     visited_2[flag_node] = 1
     while queue:
         a = queue.pop(0)
         print(a, end=' ')
-        for k in my_list[a]:
-            for kk in [k]:
-                if not visited_2[kk]:
-                    visited_2[kk] = 1
-                    queue.append(kk)
+        for k in range(len(my_list[a])):
+            if not visited_2[my_list[a][k]]:
+                visited_2[my_list[a][k]] = 1
+                queue.append(my_list[a][k])
 
 
 
@@ -31,10 +33,10 @@ my_list = [[] for _ in range(node_count+1)]
 for i in node_list:
     my_list[i[0]].append(i[1])
     my_list[i[1]].append(i[0])
-for ii in range(line_count):
+for ii in range(node_count+1):
     my_list[ii].sort()
 visited = [0]*(node_count+1)
 visited[start_node] = 1
 DFS(start_node)
-# print()
-# BFS(start_node)
+print()
+BFS(start_node)
